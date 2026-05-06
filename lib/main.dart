24c01e6edf23fd6903e056a795f7679c39dfb986
student <makhmetov_emir@mail.ru>
2026-05-06 13:40:12 +0500

    added class ArticleWidget

--- lib/main.dart
+++ lib/main.dart
@@ -55,3 +55,34 @@ class ArticleViewModel extends ChangeNotifier {
     notifyListeners();
   }
 }
+
+class ArticleWidget extends StatelessWidget {
+  final Summary summary;
+  ArticleWidget({super.key, required this.summary});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          if (summary.hasImage) Image.network(summary.originalImage!.source),
+          Text(
+            summary.titles.normalized,
+            overflow: TextOverflow.ellipsis,
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          if (summary.description != null)
+          Text(
+              summary.description!,
+              overflow: TextOverflow.ellipsis,
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+
+          Text(summary.extract),
+        ],
+      ),
+    );
+  }
+}
